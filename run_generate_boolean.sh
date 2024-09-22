ee7b0de2177777c8eb7bfcc2dfad325cdd5a63a4
quanmai <quanmai@github.com>
2024-09-22 12:23:26 -0500

    remove dupicates

--- run_generate_boolean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python generate_boolean_query.py \
-    --total-samples 50000 \
-    --num-chunks 10 \
-    --num-workers 20 \
-    --num-per-call 10 \
-    --max-tokens 4096 \
-    --boolean-type or \
